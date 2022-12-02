a073355d6b52884f96cd64f957193c361b899988
Clodoaldo Favaro <clodoaldofavaro@proton.me>
2022-12-02 00:14:42 -0300

    backup

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import { HeaderContainer } from './styles'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <h1>oi</h1>
+      <h1>oi</h1>
+      <h1>oi</h1>
+    </HeaderContainer>
+  )
+}
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  padding: 2rem 10rem;
+`
