7d49230c247b6ff45057dfd81e9f5ff6b2df199c
Clodoaldo Favaro <clodoaldofavaro@proton.me>
2023-02-23 08:12:00 -0300

    fix: corrige layout em mobile

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -6,7 +6,11 @@ export const LayoutContainer = styled.div`
   display: flex;
   flex-direction: column;
 
-  padding: 0 10rem 10rem;
+  padding: 0 2rem 2rem;
+
+  @media (min-width: 479px) {
+    padding: 0 10rem 10rem;
+  }
 
   @media (min-width: 1600px) {
     padding: 0 10rem 15rem;
